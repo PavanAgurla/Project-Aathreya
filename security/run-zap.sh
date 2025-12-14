a90170b26f42c80c03dcd308da1ec871cae5dd13
PavanAgurla <sunnypavan263@gmail.com>
2025-12-14 16:08:05 +0530

    Update OWASP ZAP Docker image version

--- security/run-zap.sh
+++ security/run-zap.sh
@@ -4,8 +4,9 @@ set -e
 mkdir -p zap-report
 
 # Run OWASP ZAP baseline scan
-docker run --rm -v $(pwd)/zap-report:/zap/wrk owasp/zap2docker-stable \
+docker run --rm -v $(pwd)/zap-report:/zap/wrk owasp/zap2docker-stable:2.12.0 \
     zap-baseline.py -t http://host.docker.internal:8080 -r /zap/wrk/report.html
+
 
 if [ -f zap-report/report.html ]; then
     echo "ZAP report generated successfully"
