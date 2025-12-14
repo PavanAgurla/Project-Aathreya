db3e16ec3427417ac2e85c17caafc127f5bf27f6
PavanAgurla <sunnypavan263@gmail.com>
2025-12-14 15:03:22 +0530

    Remove HelloControllerTest class and its test metho

--- apps/src/main/java/com/example/demo/HelloControllerTest.java
+++ apps/src/main/java/com/example/demo/HelloControllerTest.java
@@ -1,10 +1,1 @@
-package com.example.demo;
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
-public class HelloControllerTest {
-@Test
-public void contextLoadsAndHelloWorks() {
-HelloController c = new HelloController();
-assertThat(c.hello()).isEqualTo("Hello from CI/CD demo!");
-}
-}
+
