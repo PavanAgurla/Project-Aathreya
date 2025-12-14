7e39e7180eaed10c6cd8d823ba9a8eaac32c45b8
PavanAgurla <sunnypavan263@gmail.com>
2025-12-14 16:12:49 +0530

    Fix syntax error in run-zap.sh script

--- security/run-zap.sh
+++ security/run-zap.sh
@@ -4,7 +4,7 @@ set -e
 mkdir -p zap-report
 
 # Run OWASP ZAP baseline scan
-sudo docker run --rm -v $(pwd)/zap-report:/zap/wrk owasp/zap2docker-stable:2.12.0 \
+docker run --rm -v $(pwd)/zap-report:/zap/wrk owasp/zap2docker-stable:2.12.0 \
     zap-baseline.py -t http://host.docker.internal:8080 -r /zap/wrk/report.html
 
 
